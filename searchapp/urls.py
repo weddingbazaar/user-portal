ad0b909f363dc0a6625f8f3e3d2cd62568930dcf
Abhishek <singhsterabhi@gmail.com>
2016-08-01 01:29:08 +0530

    complete view and full s

--- searchapp/urls.py
+++ searchapp/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     url(r'^$', views.search, name='search'),
     url(r'^(?P<city>[a-z]+)/(?P<category>[a-z]+)/$', views.search, name='search'),
     url(r'^(?P<city>[a-z]+)/(?P<category>[a-z]+)/(?P<business>[a-zA-Z0-9\-_]+)/$', views.item, name='item'),
-
 ]
